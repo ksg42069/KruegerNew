3343af8a5df8b5391f7cba5e9dd3e983f2cda819
ksg42069 <lohitksg1295@gmail.com>
2023-05-02 22:08:47 +0530

    changed prefabs, added animations

--- KruegersTreasure2/Assets/Scripts/BookController.cs
+++ KruegersTreasure2/Assets/Scripts/BookController.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class BookController : MonoBehaviour
 {
-    //public GameObject aniDoors;
+    
     public static BookController instance;
     List<int> bookList = new List<int>();
-    // Start is called before the first frame update
+    
     void Start()
     {
-        //aniDoors = GetComponent<GameObject>();
-        //aniDoors = gameObject;
         instance = this;
-
     }
 
     // Update is called once per frame
@@ -21,7 +18,6 @@ public class BookController : MonoBehaviour
     {
         if(bookList.Count == 4)
         {
-            //aniDoors.GetComponent<Animator>().Play("opendoor");
             this.GetComponent<Animator>().Play("opendoor");
         }
     }
--- KruegersTreasure2/Assets/Scripts/OpenBook.cs
+++ KruegersTreasure2/Assets/Scripts/OpenBook.cs
@@ -4,36 +4,13 @@ using UnityEngine;
 
 public class OpenBook : MonoBehaviour
 {
-    private Animator animator;
-    public GameObject obj;
-    bool isOpen = false;
-    
-    // Start is called before the first frame update
+    private Animator animator;    
+    bool isOpen = false;   
+  
     void Start()
     {
-        animator = GetComponent<Animator>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    public void TriggerOpen()
-    {
-        //bool isOpen = animator.GetBool("openBook");
-
-        //animator.SetBool("isOpen", true);
-
-        obj.GetComponent<Animator>().Play("openBook");
-        
-    }
-    public void TriggerClose()
-    {
-        //animator.SetBool("isOpen", false);
-
-        obj.GetComponent<Animator>().Play("closeBook");
-    }
+        animator = this.GetComponent<Animator>();
+    }   
     public void ActivateTrigger()
     {
         if (!isOpen)
